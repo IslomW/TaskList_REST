03391921060b3bbf5d8976552f6d107e9f2360b7
IslomW <119986336+IslomW@users.noreply.github.com>
2024-09-25 18:32:23 +0900

    st create security part

--- src/main/java/com/sharipov/tasklist/config/ApplicationConfig.java
+++ src/main/java/com/sharipov/tasklist/config/ApplicationConfig.java
@@ -1,4 +1,38 @@
 package com.sharipov.tasklist.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+        httpSecurity
+                .csrf()
+    }
 }
