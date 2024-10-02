39e91012e9c79a610c26d8ae1b5d0fd7da9e363c
IslomW <119986336+IslomW@users.noreply.github.com>
2024-10-03 02:12:46 +0900

    Created all Jwt token part

--- src/main/java/com/sharipov/tasklist/domain/web/security/JwtUserDetailsService.java
+++ src/main/java/com/sharipov/tasklist/domain/web/security/JwtUserDetailsService.java
@@ -1,4 +1,21 @@
 package com.sharipov.tasklist.domain.web.security;
 
-public class JwtUserDetailsService {
+import com.sharipov.tasklist.domain.service.UserService;
+import com.sharipov.tasklist.domain.user.User;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+        return JwtEntityFactory.create(user);
+    }
 }
