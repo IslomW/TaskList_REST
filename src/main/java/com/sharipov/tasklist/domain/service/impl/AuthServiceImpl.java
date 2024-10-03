2486f31df64039282e7d8efc601391eea5c015d7
IslomW <119986336+IslomW@users.noreply.github.com>
2024-10-03 13:36:25 +0900

    created AuthService

--- src/main/java/com/sharipov/tasklist/domain/service/impl/AuthServiceImpl.java
+++ src/main/java/com/sharipov/tasklist/domain/service/impl/AuthServiceImpl.java
@@ -1,17 +1,38 @@
 package com.sharipov.tasklist.domain.service.impl;
 
 import com.sharipov.tasklist.domain.service.AuthService;
+import com.sharipov.tasklist.domain.service.UserService;
+import com.sharipov.tasklist.domain.user.User;
 import com.sharipov.tasklist.domain.web.dto.auth.JwtRequest;
 import com.sharipov.tasklist.domain.web.dto.auth.JwtResponse;
+import com.sharipov.tasklist.domain.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        JwtResponse jwtResponse = new JwtResponse();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        User user = userService.getByUsername(loginRequest.getUsername());
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(), user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(user.getId(), user.getUsername()));
+        return jwtResponse;
     }
 
     @Override
     public JwtResponse refresh(String refreshToken) {
-        return null;
+        return jwtTokenProvider.refreshUserTokens(refreshToken);
     }
 }
--- src/main/java/com/sharipov/tasklist/domain/web/controller/AuthController.java
+++ src/main/java/com/sharipov/tasklist/domain/web/controller/AuthController.java
@@ -45,5 +45,4 @@ public class AuthController {
         return authService.refresh(refreshToken);
     }
 
-
 }
